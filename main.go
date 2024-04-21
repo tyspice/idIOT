bb6e07f1928588e9d6ee8fb32309aa0222d83609
tyspice <tyner@tyspice.dev>
2024-04-21 11:39:05 -0700

    unmarshal config

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/tyspice/idIOT/models"
+	"gopkg.in/yaml.v2"
+)
+
+// for development
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	var cfg models.Config
+
+	homeDir, err := os.UserHomeDir()
+	check(err)
+
+	configPath := filepath.Join(homeDir, ".config/idIOT/config.yaml")
+
+	content, err := os.ReadFile(configPath)
+	check(err)
+
+	err = yaml.Unmarshal(content, &cfg)
+	check(err)
+
+	fmt.Printf("Config: %+v\n", cfg)
 
 }
