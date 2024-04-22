141405dbdf7aff4633749a873473999397ba9b2e
tyspice <tyner@tyspice.dev>
2024-04-21 22:01:29 -0700

    add dataBuffer

--- main.go
+++ main.go
@@ -5,17 +5,34 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 
 	"github.com/tyspice/idIOT/adapters"
 	"github.com/tyspice/idIOT/models"
 	"gopkg.in/yaml.v2"
 )
 
-// for development
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
+// Data Buffer
+type dataBuffer struct {
+	mu   sync.Mutex
+	data []models.DataPoint
+}
+
+func newDataBuffer() *dataBuffer {
+	return &dataBuffer{}
+}
+
+func (b *dataBuffer) enqueue(dp models.DataPoint) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.data = append(b.data, dp)
+}
+
+func (b *dataBuffer) flushToChannel(out chan<- []models.DataPoint) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	out <- b.data
+	b.data = b.data[:0]
 }
 
 func main() {
@@ -32,7 +49,9 @@ func main() {
 	err = yaml.Unmarshal(content, &cfg)
 	check(err)
 
-	receiveChan := make(chan models.DataPoint, 5)
+	buffer := newDataBuffer()
+	receiveChan := make(chan models.DataPoint, 10)
+	flushChan := make(chan []models.DataPoint, 5)
 
 	var wg sync.WaitGroup
 
@@ -44,12 +63,35 @@ func main() {
 			if !ok {
 				break
 			}
-			fmt.Printf("%+v\n", dp)
+			buffer.enqueue(dp)
 		}
 		wg.Done()
+	}()
+
+	go func() {
+		for {
+			time.Sleep(time.Duration(cfg.FlushInterval) * time.Second)
+			buffer.flushToChannel(flushChan)
+		}
+	}()
+
+	go func() {
+		for {
+			flush := <-flushChan
+			for _, dp := range flush {
+				fmt.Printf("%+v\n", dp)
+			}
+		}
 	}()
 
 	broker := adapters.NewBroker(&cfg)
 	broker.Subscribe(cfg, receiveChan)
 	wg.Wait()
+}
+
+// for development
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
